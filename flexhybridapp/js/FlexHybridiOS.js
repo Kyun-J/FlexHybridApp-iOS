597b7d738aa8f7f4b913c999e93f32724acc900f
Kyun-J <dvkyun@gmail.com>
2020-04-28 20:11:16 +0900

    no message

--- flexhybridapp/js/FlexHybridiOS.js
+++ flexhybridapp/js/FlexHybridiOS.js
@@ -8,7 +8,7 @@ const logs = { log: console.log, debug: console.debug, error: console.error, inf
 window.$flex = {};
 Object.defineProperties($flex,
     {
-        version: { value: '0.1.2.1', writable: false },
+        version: { value: '0.1.2.2', writable: false },
         addEventListener: { value: function(event, callback) { events.push({ e: event, c: callback }) }, writable: false },
         init: { value: function() { window.Function(script)(); }, writable: false },
         web: { value: {}, writable: false }
