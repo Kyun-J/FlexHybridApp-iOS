4748563adaf91d7437f9fbdecbf30c941cb0cac5
Kyun-J <dvkyun@gmail.com>
2020-11-09 15:06:08 +0900

    0.6.3 publish

--- framework/flexhybridapp/FlexClosure.swift
+++ framework/flexhybridapp/FlexClosure.swift
@@ -14,31 +14,31 @@ public struct FlexClosure {
         return closure
     }
     
-    static public func int(_ closure: @escaping (_ arguments: Array<FlexData>) throws -> Int) -> (_ arguments: Array<FlexData>) throws -> Int {
+    static public func int(_ closure: @escaping (_ arguments: Array<FlexData>) throws -> Int?) -> (_ arguments: Array<FlexData>) throws -> Int? {
         return closure
     }
     
-    static public func double(_ closure: @escaping (_ arguments: Array<FlexData>) throws -> Double) -> (_ arguments: Array<FlexData>) throws -> Double {
+    static public func double(_ closure: @escaping (_ arguments: Array<FlexData>) throws -> Double?) -> (_ arguments: Array<FlexData>) throws -> Double? {
         return closure
     }
     
-    static public func float(_ closure: @escaping (_ arguments: Array<FlexData>) throws -> Float) -> (_ arguments: Array<FlexData>) throws -> Float {
+    static public func float(_ closure: @escaping (_ arguments: Array<FlexData>) throws -> Float?) -> (_ arguments: Array<FlexData>) throws -> Float? {
         return closure
     }
     
-    static public func bool(_ closure: @escaping (_ arguments: Array<FlexData>) throws -> Bool) -> (_ arguments: Array<FlexData>) throws -> Bool {
+    static public func bool(_ closure: @escaping (_ arguments: Array<FlexData>) throws -> Bool?) -> (_ arguments: Array<FlexData>) throws -> Bool? {
         return closure
     }
     
-    static public func string(_ closure: @escaping (_ arguments: Array<FlexData>) throws -> String) -> (_ arguments: Array<FlexData>) throws -> String {
+    static public func string(_ closure: @escaping (_ arguments: Array<FlexData>) throws -> String?) -> (_ arguments: Array<FlexData>) throws -> String? {
         return closure
     }
     
-    static public func array(_ closure: @escaping (_ arguments: Array<FlexData>) throws -> Array<Any?>) -> (_ arguments: Array<FlexData>) throws -> Array<Any?> {
+    static public func array(_ closure: @escaping (_ arguments: Array<FlexData>) throws -> Array<Any?>?) -> (_ arguments: Array<FlexData>) throws -> Array<Any?>? {
         return closure
     }
     
-    static public func dictionary(_ closure: @escaping (_ arguments: Array<FlexData>) throws -> Dictionary<String, Any?>) -> (_ arguments: Array<FlexData>) throws -> Dictionary<String, Any?> {
+    static public func dictionary(_ closure: @escaping (_ arguments: Array<FlexData>) throws -> Dictionary<String, Any?>?) -> (_ arguments: Array<FlexData>) throws -> Dictionary<String, Any?>? {
         return closure
     }
     
